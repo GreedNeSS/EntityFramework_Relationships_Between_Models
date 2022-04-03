38a2868924f7929cb1f3700ec5374a131da384fa
GreedNeSS <ruslan199126@gmail.com>
2022-04-03 15:12:43 +0300

    Working with foraign keys and navigation propertys.

---
+++ FOREIGN_KEY_And_Navigation_Property/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FOREIGN_KEY_And_Navigation_Property.Models;
+
+namespace FOREIGN_KEY_And_Navigation_Property
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Employee.db");
+        }
+    }
+}
--- FOREIGN_KEY_And_Navigation_Property/Program.cs
+++ FOREIGN_KEY_And_Navigation_Property/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FOREIGN_KEY_And_Navigation_Property;
+using FOREIGN_KEY_And_Navigation_Property.Models;
+
+Console.WriteLine("***** FOREIGN KEY And Navigation Property *****");
+
+Console.WriteLine("\nПервый пример: ");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Company microsoft = new Company { Title = "Microsoft" };
+    Company apple = new Company { Title = "Apple" };
+    db.Companies.AddRange(microsoft, apple);
+    db.SaveChanges();
+
+    db.Users.AddRange(new User { Name = "Ruslan", Company = microsoft },
+        new User { Name = "Marcus", Company = microsoft },
+        new User { Name = "Henry", Company = apple });
+    db.SaveChanges();
+
+    List<Company> companies = db.Companies.ToList();
+    foreach (var company in companies)
+    {
+        List<string>? names = company.Users?.Select(u => u?.Name ?? "Имя отсутствует").ToList<string>();
+
+        if (names is not null)
+        {
+            Console.WriteLine($"\nCompany: {company.Title}, " +
+            $"\nEmployee: {string.Join(", ", names)}");
+        }
+    }
+}
+
+Console.WriteLine("\nВторой пример: ");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User ruslan = new User { Name = "Ruslan" };
+    User marcus = new User { Name = "Marcus" };
+    User henry = new User { Name = "Henry" };
+
+    Company yandex = new Company { Title = "Yandex", Users = { ruslan, marcus } };
+    Company jetBrains = new Company { Title = "JetBrains", Users = { henry } };
+    db.Companies.AddRange(yandex, jetBrains);
+    db.Users.AddRange(ruslan, marcus, henry);
+    db.SaveChanges();
+
+    List<User> users = db.Users.ToList();
+    foreach (var user in users)
+    {
+        Console.WriteLine($"\n{user.Name} работает в компании {user?.Company?.Title ?? "unknown"}");
+    }
+}
