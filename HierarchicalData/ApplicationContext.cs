3fc76b4826d7840390c8d7ca5243d2101835f4a8
GreedNeSS <ruslan199126@gmail.com>
2022-04-08 16:39:27 +0300

    Added hierarchical data example.

---
+++ HierarchicalData/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HierarchicalData
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<MenuItem> MenuItems { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=Menu.db");
+        }
+    }
+}
---
+++ HierarchicalData/Program.cs
@@ -0,0 +1,47 @@
+﻿using HierarchicalData;
+
+Console.WriteLine("***** Hierarchical Data *****");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    MenuItem file = new MenuItem { Title = "File" };
+    MenuItem edit = new MenuItem { Title = "Edit" };
+    
+    MenuItem open = new MenuItem { Title = "Open", Parent = file };
+    MenuItem save = new MenuItem { Title = "Save", Parent = file };
+    
+    MenuItem copy = new MenuItem { Title = "Copy", Parent = edit };
+    MenuItem paste = new MenuItem { Title = "Paste", Parent = edit };
+
+    db.MenuItems.AddRange(file, edit, open, save, copy, paste);
+    db.SaveChanges();
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Console.WriteLine("\n=> All items:");
+
+    foreach (var item in db.MenuItems.ToList())
+    {
+        Console.WriteLine(item.Title);
+    }
+
+    var file = db.MenuItems.FirstOrDefault(i => i.Title == "File");
+    var edit = db.MenuItems.FirstOrDefault(i => i.Title == "Edit");
+
+    ShowChildren(file);
+    ShowChildren(edit);
+}
+
+void ShowChildren(MenuItem? item)
+{
+    if (item is not null)
+    {
+        Console.WriteLine($"\n=>{item?.Title}:");
+
+        foreach (var i in item.Chidren)
+        {
+            Console.WriteLine($"- {i.Title}");
+        }
+    }
+}
